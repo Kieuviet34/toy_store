20541f500152f04258fa6a8afd31392f1a2ef998
Kieuviet34 <kieuviet34@gmail.com>
2025-01-21 07:16:06 +0700

    2nd commnit

---
+++ app/views/homepage.php
@@ -0,0 +1,9 @@
+<?php include("public/index.php"); ?>
+<div id="top" class="">
+    <div class="row row-cols-3">
+        
+    </div>
+</div>
+<div id="home-content">
+
+</div>
---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+    $server = 'localhost';
+    $name = 'root';
+    $password = 'Kieuviet2004@';
+    $database = 'toy_store';
+
+    $conn = new mysqli($server, $name, $password, $database);
+
+    
+?>
