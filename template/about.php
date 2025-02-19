6e670f3cc97cc9a8f59102c71f161f415edd90cd
mikedang123 <jliu22183@gmail.com>
2025-02-20 00:06:37 +0700

    Changed somethingg

--- template/about.php
+++ template/about.php
@@ -1,2 +1,4 @@
-
-    <!-- viết giao diện vào đây -->
+<div class="container mt-5 text-center">
+    <h1 class="display-4">About Us</h1>
+    <p class="lead">Chào mừng đến với cửa hàng mô hình của 3 anh em nịnh thần. Thứ gì bạn cần chúng tôi không bán !!</p>
+</div>
--- template/contact.php
+++ template/contact.php
@@ -1,3 +1,26 @@
 
-    <!-- viết giao diện vào đây -->
-    <h1>contact</h1>
+<div class="container mt-5 text-center">
+    <h1 class="display-4">Contact Us</h1>
+    <p class="lead">Nếu sản phẩm có bất kỳ vấn đề nào, làm ơn đừng liên hệ với chúng tôi !!!<a href="mailto:contact@toystore.com">3anhemninhthan@saygex.com</a> hoặc gọi ngay <strong>(123) 456-7890</strong>.</p>
+    
+    <div class="row mt-5 justify-content-center">
+        <div class="col-md-3">
+            <div class="card border-0 shadow-lg p-3">
+                <img src="template/img/domixue.jpg" class="rounded-circle img-fluid" alt="Độ Mixue">
+                <h5 class="mt-3">Độ Mixue</h5>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card border-0 shadow-lg p-3">
+                <img src="template/img/doskibidi.jpg" class="rounded-circle img-fluid" alt="Độ Skibidi">
+                <h5 class="mt-3">Độ Skibidi</h5>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card border-0 shadow-lg p-3">
+                <img src="template/img/dojolibe.jpg" class="rounded-circle img-fluid" alt="Độ Jolibee">
+                <h5 class="mt-3">Độ Jolibee</h5>
+            </div>
+        </div>
+    </div>
+</div>
