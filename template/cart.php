32301e60335eaa02f9f6757d269b55f11ddf76c9
mikedang123 <jliu22183@gmail.com>
2025-02-24 20:16:20 +0700

    thêm product+ checkout

--- template/cart.php
+++ template/cart.php
@@ -1,2 +1,29 @@
 
-    <!-- viết giao diện vào đây -->
+
+<div class="container mt-5">
+    <h1 class="display-4 text-center">Giỏ Hàng</h1>
+    
+    <table class="table table-bordered mt-4">
+        <thead class="thead-dark">
+            <tr>
+                <th>Sản Phẩm</th>
+                <th>Giá</th>
+                <th>Số Lượng</th>
+                <th>Tổng</th>
+                <th>Hành Động</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- Các mặt hàng trong giỏ sẽ được hiển thị ở đây động -->
+        </tbody>
+    </table>
+    
+    <div class="text-right mt-4">
+        <h4>Tổng Cộng: <span id="cart-total">0₫</span></h4>
+        <a href="checkout.php" class="btn btn-success">Tiến Hành Thanh Toán</a>
+    </div>
+</div>
+
+<?php
+include 'footer.php';
+?>
