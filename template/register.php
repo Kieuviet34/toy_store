dbbb8ef4386fc7b35d24dfdb93a4de5c96a7f6c5
Kieuviet34 <kieuviet34@gmail.com>
2025-03-18 02:05:05 +0700

    change style register

--- template/register.php
+++ template/register.php
@@ -2,7 +2,7 @@
     <div class="row">
       <div class="col-md-4 offset-md-4 ">
         <div class="card my-5">
-          <div class="card-body px-lg-8">
+          <div class="card-body px-lg-8" style="background-color:  #ebf2fa;">
             <h2 class="text-center mb-4">Tạo tài khoản</h2>
 
             <!-- Profile Image -->
